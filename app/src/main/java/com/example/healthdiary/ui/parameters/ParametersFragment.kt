3e7f90518e65713a19edd608fafc52bab7f3a563
ilya veselov <ves.ilya@mail.ru>
2020-03-30 21:52:52 +0300

    Added birth date and sex params, updated params page

--- app/src/main/java/com/example/healthdiary/ui/parameters/ParametersFragment.kt
+++ app/src/main/java/com/example/healthdiary/ui/parameters/ParametersFragment.kt
@@ -1,5 +1,6 @@
 package com.example.healthdiary.ui.parameters
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +10,11 @@ import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.healthdiary.R
 import com.example.healthdiary.db.DBHelper
+import com.example.healthdiary.db.UserParameter
 import com.example.healthdiary.db.UserParameters
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.pow
 
 class ParametersFragment : Fragment() {
@@ -21,42 +26,115 @@ class ParametersFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_parameters, container, false)
 
-        val dbHandler = DBHelper(container!!.context, null)
+        //params controls
         val inputWeight: EditText = root.findViewById(R.id.input_weight)
         val inputHeight: EditText = root.findViewById(R.id.input_height)
-        val lastUserParameters = dbHandler.getLastUserParameters()
         val hintText: TextView = root.findViewById(R.id.hint_text)
         val resultsTable: TableLayout = root.findViewById(R.id.results_table)
+        val ageText: TextView = root.findViewById(R.id.age_text)
+        val sexSpinner: Spinner = root.findViewById(R.id.sex_spinner)
+
+        //db
+        val dbHandler = DBHelper(container!!.context, null)
+        val lastUserParameters = dbHandler.getLastUserParameters()
         if (lastUserParameters != null) {
             inputWeight.setText(lastUserParameters.weight.toString())
             inputHeight.setText(lastUserParameters.height.toString())
-            hintText.text = getHint(lastUserParameters)
             val userParametersList = dbHandler.getUserParametersList()
             fillTable(userParametersList, resultsTable, container!!.context)
         }
+        val c = Calendar.getInstance()
+        var birthDate = ""
+        var birthYear: Int = c.get(Calendar.YEAR)
+        var birthMonth = c.get(Calendar.MONTH)
+        var birthDay = c.get(Calendar.DAY_OF_MONTH)
+        val userParameterList = dbHandler.getUserParameterList()
+        for (userParameter in userParameterList) {
+            if (userParameter.name == "birthDate") {
+                birthDate = userParameter.value
+                val dateParams = birthDate.split(".").toTypedArray()
+                birthDay = dateParams[0].toInt()
+                birthMonth = dateParams[1].toInt()
+                birthYear = dateParams[2].toInt()
+                var age = getAge(birthDay, birthMonth, birthYear)
+                if (age >= 0) {
+                    ageText.text = "Your age is $age"
+                }
+            } else if (userParameter.name == "sex") {
+                val adapter = ArrayAdapter(container!!.context, R.layout.fragment_parameters ,container!!.context.resources.getStringArray(R.array.sex_list))
+                sexSpinner.setSelection(adapter.getPosition(userParameter.value))
+            } else if (userParameter.name == "massIndex") {
+                hintText.text = getHint(userParameter.value.toFloat())
+            }
+        }
 
+        //birth date
+        val chooseDateButton: Button = root.findViewById(R.id.button_choose_date)
+        chooseDateButton.setOnClickListener {
+            val dpd = DatePickerDialog(container!!.context, DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                var age = getAge(dayOfMonth, monthOfYear, year)
+                if (age >= 0) {
+                    ageText.text = "Your age is $age"
+                }
+
+                birthDate = "$dayOfMonth.$monthOfYear.$year"
+
+                birthYear = year
+                birthMonth = monthOfYear
+                birthDay = dayOfMonth
+            }, birthYear, birthMonth, birthDay)
+
+            dpd.show()
+        }
+
+        //save
         val saveButton: Button = root.findViewById(R.id.button_save)
         saveButton.setOnClickListener {
             val weight = inputWeight.text.toString()
             val height = inputHeight.text.toString()
-            if (weight == "" || height == "") {
+            if (weight == "" || height == "" || birthDate == "") {
                 Toast.makeText(container!!.context, "Fill empty inputs!", Toast.LENGTH_LONG).show()
             } else {
-                val userParameters = UserParameters(weight.toFloat(), height.toFloat())
-                dbHandler.insertUserParameter(userParameters)
-                Toast.makeText(container!!.context, "New values were saved!", Toast.LENGTH_LONG)
-                    .show()
+                dbHandler.updateUserParameter(UserParameter("birthDate", birthDate))
 
-                hintText.text = getHint(userParameters)
+                dbHandler.updateUserParameter(UserParameter("sex", sexSpinner.selectedItem.toString()))
+
+                val userParameters = UserParameters(weight.toFloat(), height.toFloat())
+                dbHandler.insertUserParameters(userParameters)
+
+                val massIndex = getMassIndex(userParameters)
+                dbHandler.updateUserParameter(UserParameter("massIndex", massIndex.toString()))
+                hintText.text = getHint(massIndex)
+
                 val result = dbHandler.getUserParametersList()
                 fillTable(result, resultsTable, container!!.context)
+
+                Toast.makeText(container!!.context, "New values were saved!", Toast.LENGTH_LONG)
+                    .show()
             }
         }
+
         return root
     }
 
-    private fun getHint(userParameters: UserParameters): String {
-        val massIndex = userParameters.weight / (userParameters.height / 100f).pow(2)
+    private fun getAge(day: Int, month: Int, year: Int): Int {
+        val currentDate = Calendar.getInstance()
+        val currentYear = currentDate.get(Calendar.YEAR)
+        val currentMonth = currentDate.get(Calendar.MONTH)
+        val currentDay = currentDate.get(Calendar.DAY_OF_MONTH)
+        var age = currentYear - year
+        if (currentMonth < month || currentMonth == month && currentDay < day)
+        {
+            age--
+        }
+        return age
+    }
+
+    private fun getMassIndex(userParameters: UserParameters): Float {
+        return userParameters.weight / (userParameters.height / 100f).pow(2)
+    }
+
+    private fun getHint(massIndex: Float): String {
         return when {
             massIndex < 18.5f -> "The deficit of body weight"
             massIndex < 25 -> "Normal body weight"
