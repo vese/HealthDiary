b6444c3212462d6edf44c2ebe528228f2c96b63d
CarotisInterna <interna.carotis@gmail.com>
2020-04-04 09:52:49 +0300

    extract mass index constants, rename hints

---
+++ app/src/main/java/com/example/healthdiary/util/MassIndexConstants.kt
@@ -0,0 +1,7 @@
+package com.example.healthdiary.util
+
+const val DEFICIT = 18.5f
+const val NORMAL = 25f
+const val EXCESS = 30f
+const val FIRST_DEGREE_OBESITY = 35f
+const val SECOND_DEGREE_OBESITY = 40f
